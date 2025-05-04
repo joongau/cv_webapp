57e10bc0ded7c6cecc5b98159b0f0d7cb02adb71
Jonathan <gaultier.jonathan@gmail.com>
2025-05-04 17:09:54 +0200

    add projects route

--- app/routes.py
+++ app/routes.py
@@ -43,3 +43,19 @@ def home():
         }
     }
     return render_template("home.html", cv=cv_data)
+
+@main.route('/projets')
+def projets():
+    projets_data = [
+        {
+            "titre": "Mobrange",
+            "description": "Application web de recommandation de smartphones basée sur les habitudes de vie des clients. Combine mes connaissances terrain et l’IA.",
+            "année": "2025"
+        },
+        {
+            "titre": "Clairdelunestudio.com",
+            "description": "E-commerce de produits artisanaux personnalisés (affiches avec dorure), développé sur Shopify.",
+            "année": "2023 – Aujourd’hui"
+        }
+    ]
+    return render_template("projets.html", projets=projets_data)
